deb38828db81762559c2b1d5f4b9ecf61f5ec096
william <willshinji@gmail.com>
2018-04-25 01:30:57 -0300

    wow m more tests

--- spec/VehicleTest.js
+++ spec/VehicleTest.js
@@ -9,6 +9,17 @@ describe('VehicleTest', () => {
   describe('#new', () => {
     it('should create a new vehicle', () => {
       expect(vehicle.model).not.toBeUndefined();
+      expect(vehicle.model).toEqual('Ferrari 458 Italia');
+    })
+  })
+
+  describe('#info', () => {
+    it('should display Ferrari 458 Italia 2016 ', () => {
+      spyOn(vehicle, 'info');
+      vehicle.info();
+      expect(vehicle.info).toHaveBeenCalled();
+      expect(vehicle.info.calls.count()).toEqual(1);
+      
     })
   })
 })
